ff79d46af9e9a460c807c11f85c7703ecc3c4a0d
IvanLuLyf <835498692@qq.com>
2019-02-17 18:56:51 +0800

    Fix Query Bug

--- app/model/QqBindModel.php
+++ app/model/QqBindModel.php
@@ -10,7 +10,7 @@ class QqBindModel extends Model
 {
     public function getAvatar($key, $uid)
     {
-        if ($bind_row = $this->where(['uid=:uid'], [':uid' => $uid])->fetch()) {
+        if ($bind_row = $this->where(['uid=:uid'], ['uid' => $uid])->fetch()) {
             $bind_uid = $bind_row['buid'];
             return "http://qzapp.qlogo.cn/qzapp/$key/$bind_uid/100";
         } else {
