0958d6148ee8e9508b62776d0222b15abd64ca43
IvanLuLyf <835498692@qq.com>
2019-04-11 02:43:06 +0800

    Update NotifyController

--- app/controller/NotifyController.php
+++ app/controller/NotifyController.php
@@ -14,10 +14,7 @@ class NotifyController extends Controller
     function ac_view()
     {
         $tp_user = BunnyPHP::app()->get('tp_user');
-        $this->assign('ret', 0);
-        $this->assign('status', 'ok');
         $notice = (new NotificationModel())->getNotice($tp_user['uid']);
-        $this->assign('notifications', $notice);
-        $this->render();
+        $this->assignAll(['ret' => 0, 'status' => 'ok', 'notifications' => $notice])->render();
     }
 }
