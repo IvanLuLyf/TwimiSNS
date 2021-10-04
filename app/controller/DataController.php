634efd8cd2cef0bf76a0e03b2471cbddf5d12001
IvanLuLyf <835498692@qq.com>
2021-10-04 22:42:12 +0800

    simplify

--- app/controller/DataController.php
+++ app/controller/DataController.php
@@ -4,7 +4,6 @@ use BunnyPHP\BunnyPHP;
 use BunnyPHP\Controller;
 
 /**
- * Created by PhpStorm.
  * User: IvanLu
  * Date: 2019/3/15
  * Time: 16:42
@@ -14,23 +13,23 @@ class DataController extends Controller
     /**
      * @filter auth canGetInfo
      */
-    public function ac_get()
+    public function ac_get(DataModel $dataModel)
     {
         $tp_api = BunnyPHP::app()->get('tp_api');
         $tp_user = BunnyPHP::app()->get('tp_user');
-        $content = (new DataModel())->get($tp_user['uid'], $tp_api['id']);
-        if ($content == null) $content = "";
-        $this->assignAll(['ret' => 0, 'status' => 'ok', 'content' => $content])->render();
+        $content = $dataModel->get($tp_user['uid'], $tp_api['id']);
+        if ($content == null) $content = '';
+        return ['ret' => 0, 'status' => 'ok', 'content' => $content];
     }
 
     /**
      * @filter auth canGetInfo
      */
-    public function ac_set()
+    public function ac_set(DataModel $dataModel)
     {
         $tp_api = BunnyPHP::app()->get('tp_api');
         $tp_user = BunnyPHP::app()->get('tp_user');
-        (new DataModel())->set($tp_user['uid'], $tp_api['id'], $_POST['content']);
-        $this->assignAll(['ret' => 0, 'status' => 'ok'])->render();
+        $dataModel->set($tp_user['uid'], $tp_api['id'], $_POST['content']);
+        return ['ret' => 0, 'status' => 'ok'];
     }
 }
