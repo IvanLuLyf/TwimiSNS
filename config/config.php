27cda5d5b7eb8f1fdc307eec8fe744e07e555df0
IvanLuLyf <835498692@qq.com>
2021-10-04 17:28:14 +0800

    config using env

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'db' => [
+        'prefix' => 'tp_',
+        'dsn' => $_ENV['DATABASE_DSN'] ?? '',
+        'type' => $_ENV['DATABASE_TYPE'] ?? 'sqlite',
+        'database' => $_ENV['DATABASE_NAME'] ?? 'sns.sqlite3',
+        'host' => $_ENV['DATABASE_HOST'] ?? '',
+        'username' => $_ENV['DATABASE_USERNAME'] ?? '',
+        'password' => $_ENV['DATABASE_PASSWORD'] ?? '',
+    ],
+    'site_name' => $_ENV['BUNNY_SITE_NAME'] ?? 'TwimiSNS',
+    'site_url' => $_ENV['BUNNY_SITE_URL'] ?? 'localhost',
+    'controller' => 'Index',
+    'action' => 'index',
+    'allow_reg' => $_ENV['BUNNY_ALLOW_REG'] ?? '1',
+];
