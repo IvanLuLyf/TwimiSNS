d3033e7e868ca272a91f08682aa007e03400be4b
IvanLuLyf <835498692@qq.com>
2021-10-07 18:22:45 +0800

    add beta

--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -4,19 +4,23 @@ use BunnyPHP\Config;
 use BunnyPHP\Controller;
 
 /**
- * Created by PhpStorm.
  * User: IvanLu
  * Date: 2018/7/28
  * Time: 18:17
  */
 class IndexController extends Controller
 {
-    function ac_index()
+    public function ac_index()
     {
         if (Config::check('config')) {
             $this->redirect('post', 'list');
         } else {
             $this->redirect('install', 'index');
         }
+    }
+
+    public function ac_beta()
+    {
+        $this->render('index.php');
     }
 }
