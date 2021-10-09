a4d0bcbe9fda07f144143126f64518551b8bfa25
IvanLuLyf <835498692@qq.com>
2021-10-10 01:28:51 +0800

    update FeedController

--- app/controller/FeedController.php
+++ app/controller/FeedController.php
@@ -4,188 +4,157 @@ use BunnyPHP\BunnyPHP;
 use BunnyPHP\Controller;
 
 /**
- * Created by PhpStorm.
- * User: IvanLu
- * Date: 2018/1/1
- * Time: 22:57
+ * @author IvanLu
+ * @time 2018/1/1 22:57
+ * @filter auth canFeed
  */
 class FeedController extends Controller
 {
+    private FeedModel $feedModel;
     /**
-     * @filter auth canFeed
+     * @var mixed|null
      */
-    public function ac_send()
+    private $user;
+
+    public function __construct(FeedModel $feedModel)
     {
-        if (BUNNY_APP_MODE == BunnyPHP::MODE_API) {
-            if (isset($_POST['content'])) {
-                $imageCount = 0;
-                if (isset($_FILES['images'])) {
-                    $paths = $_FILES['images']["tmp_name"];
-                    if (is_array($paths)) {
-                        $imageCount = count($paths);
-                    } else {
-                        $imageCount = 1;
-                    }
-                }
-                $tp_user = BunnyPHP::app()->get('tp_user');
-                $tp_api = BunnyPHP::app()->get('tp_api');
-                $feedId = (new FeedModel())->sendFeed($tp_user, $_POST['content'], $tp_api['name'], $imageCount);
-                $feedImageModel = new FeedImageModel();
-                if ($imageCount > 0) {
-                    $image_type = ['image/bmp', 'image/gif', 'image/jpeg', 'image/pjpeg', 'image/png', 'application/x-bmp', 'application/x-jpg', 'application/x-png'];
-                    for ($i = 0; $i < $imageCount; $i++) {
-                        if (in_array($_FILES["images"]["type"][$i], $image_type) && ($_FILES["images"]["size"][$i] < 2000000)) {
-                            $t = time() % 1000;
-                            $filename = "feed/$feedId-$i-$t.jpg";
-                            $url = BunnyPHP::getStorage()->upload($filename, $_FILES["images"]["tmp_name"][$i]);
-                            $feedImageModel->upload($tp_user['uid'], $feedId, $url);
-                        }
-                    }
-                }
-                $this->assignAll(['ret' => 0, 'status' => 'ok', 'tid' => $feedId]);
+        $this->user = BunnyPHP::app()->get('tp_user');
+        $this->feedModel = $feedModel;
+    }
+
+    public function ac_send(): array
+    {
+        if (isset($_POST['content'])) {
+            return ['ret' => -7, 'status' => 'parameter cannot be empty'];
+        }
+        $imageCount = 0;
+        if (isset($_FILES['images'])) {
+            $paths = $_FILES['images']["tmp_name"];
+            if (is_array($paths)) {
+                $imageCount = count($paths);
             } else {
-                $this->assignAll(['ret' => -7, 'status' => 'parameter cannot be empty']);
+                $imageCount = 1;
             }
         }
-        $this->render();
+        $tp_api = BunnyPHP::app()->get('tp_api');
+        $feedId = $this->feedModel->sendFeed($this->user, $_POST['content'], $tp_api['name'], $imageCount);
+        $feedImageModel = new FeedImageModel();
+        if ($imageCount > 0) {
+            $image_type = ['image/bmp', 'image/gif', 'image/jpeg', 'image/pjpeg', 'image/png', 'application/x-bmp', 'application/x-jpg', 'application/x-png'];
+            for ($i = 0; $i < $imageCount; $i++) {
+                if (in_array($_FILES["images"]["type"][$i], $image_type) && ($_FILES["images"]["size"][$i] < 2000000)) {
+                    $t = time() % 1000;
+                    $filename = "feed/$feedId-$i-$t.jpg";
+                    $url = BunnyPHP::getStorage()->upload($filename, $_FILES["images"]["tmp_name"][$i]);
+                    $feedImageModel->upload($this->user['uid'], $feedId, $url);
+                }
+            }
+        }
+        return ['ret' => 0, 'status' => 'ok', 'tid' => $feedId];
     }
 
     /**
-     * @filter auth canFeed
      * @param int $tid path(0)
      * @param int $page path(1)
      */
-    public function ac_view(int $tid = 0, int $page = 1)
+    public function ac_view(int $tid = 0, int $page = 1): array
     {
-        if (BUNNY_APP_MODE == BunnyPHP::MODE_API) {
-            $tp_user = BunnyPHP::app()->get('tp_user');
-            $this->assignAll(['ret' => 0, 'status' => 'ok', 'page' => $page]);
-            if ($tid == 0) {
-                $feeds = (new FeedModel())->listFeed($tp_user['uid'], $page);
-                foreach ($feeds as &$feed) {
-                    if ($feed['image'] != null && $feed['image'] > 0) {
-                        $feed['images'] = (new FeedImageModel())->getFeedImageByTid($feed['tid']);
-                    }
-                }
-                $note_info = (new NotificationModel())->getUnreadCnt($tp_user['uid']);
-                $user_info = (new UserInfoModel())->get($tp_user['uid']);
-                $this->assignAll(['tid' => $tid, 'note_count' => $note_info[0], 'note_uid' => $note_info[1], 'user_info' => $user_info, 'feeds' => $feeds]);
-            } else {
-                $feed = (new FeedModel())->getFeed($tid);
-                $noteName = (new FriendModel())->getNoteNameByUsername($tp_user['uid'], $feed['username']);
-                $feed['notename'] = $noteName;
+        $res = ['ret' => 0, 'status' => 'ok', 'page' => $page];
+        if ($tid == 0) {
+            $feeds = $this->feedModel->listFeed($this->user['uid'], $page);
+            foreach ($feeds as &$feed) {
                 if ($feed['image'] != null && $feed['image'] > 0) {
                     $feed['images'] = (new FeedImageModel())->getFeedImageByTid($feed['tid']);
                 }
-                $comments = (new CommentModel())->listComment($tid, 3, $page, $tp_user['uid']);
-                $this->assignAll(['feed' => $feed, 'comments' => $comments]);
             }
+            $note_info = (new NotificationModel())->getUnreadCnt($this->user['uid']);
+            $user_info = (new UserInfoModel())->get($this->user['uid']);
+            return array_merge($res, ['tid' => $tid, 'note_count' => $note_info[0], 'note_uid' => $note_info[1], 'user_info' => $user_info, 'feeds' => $feeds]);
+        } else {
+            $feed = $this->feedModel->getFeed($tid);
+            $noteName = (new FriendModel())->getNoteNameByUsername($this->user['uid'], $feed['username']);
+            $feed['notename'] = $noteName;
+            if ($feed['image'] != null && $feed['image'] > 0) {
+                $feed['images'] = (new FeedImageModel())->getFeedImageByTid($feed['tid']);
+            }
+            $comments = (new CommentModel())->listComment($tid, ConstUtil::MOD_FEED, $page, $this->user['uid']);
+            return array_merge($res, ['feed' => $feed, 'comments' => $comments]);
         }
-        $this->render();
     }
 
     /**
-     * @filter auth canFeed
      * @param string $username path(0)
      * @param int $page path(1)
      */
-    public function ac_list(string $username = '', int $page = 1)
+    public function ac_list(string $username = '', int $page = 1): array
     {
         $user = (new UserModel())->getUserByUsername($username);
-        if (BUNNY_APP_MODE == BunnyPHP::MODE_API) {
-            if ($user['uid'] != null) {
-                $feeds = (new FeedModel())->userFeed($username, $page);
-                foreach ($feeds as &$feed) {
-                    if ($feed['image'] != null && $feed['image'] > 0) {
-                        $feed['images'] = (new FeedImageModel())->getFeedImageByTid($feed['tid']);
-                    }
-                }
-                $user_info = (new UserInfoModel())->get($user['uid']);
-                $this->assignAll(['ret' => 0, 'status' => 'ok', 'page' => $page, 'user_info' => $user_info, 'feeds' => $feeds]);
-            } else {
-                $this->assignAll(['ret' => 1002, 'status' => "user does not exist"]);
+        if (!$user['uid']) {
+            return ['ret' => 1002, 'status' => "user does not exist"];
+        }
+        $feeds = $this->feedModel->userFeed($username, $page);
+        foreach ($feeds as &$feed) {
+            if ($feed['image'] != null && $feed['image'] > 0) {
+                $feed['images'] = (new FeedImageModel())->getFeedImageByTid($feed['tid']);
             }
         }
-        $this->render();
+        $user_info = (new UserInfoModel())->get($user['uid']);
+        return ['ret' => 0, 'status' => 'ok', 'page' => $page, 'user_info' => $user_info, 'feeds' => $feeds];
     }
 
     /**
-     * @filter auth canFeed
      * @param int $tid path(0)
      */
-    public function ac_comment(int $tid = 0)
+    public function ac_comment(int $tid = 0): array
     {
-        if (BUNNY_APP_MODE == BunnyPHP::MODE_API) {
-            if (isset($_POST['content'])) {
-                if ($feed = (new FeedModel())->getFeed($tid)) {
-                    $tp_user = BunnyPHP::app()->get('tp_user');
-                    $comment_id = (new CommentModel())->sendComment($tid, 3, $tp_user, $_POST['content']);
-                    (new NotificationModel())->notify(3, $tid, $feed['uid'], $tp_user['uid'], 'comment', $_POST['content']);
-                    $this->assignAll(['ret' => 0, 'status' => 'ok', 'tid' => $tid, 'cid' => $comment_id]);
-                } else {
-                    $this->assignAll(['ret' => 3001, 'status' => 'invalid tid']);
-                }
-            } else {
-                $this->assignAll(['ret' => -7, 'status' => 'parameter cannot be empty']);
-            }
+        if (StrUtil::emptyText($_POST['content'])) {
+            return ['ret' => -7, 'status' => 'parameter cannot be empty'];
         }
-        $this->render();
+        if ($feed = $this->feedModel->getFeed($tid)) {
+            $comment_id = (new CommentModel())->sendComment($tid, ConstUtil::MOD_FEED, $this->user, $_POST['content']);
+            (new NotificationModel())->notify(3, $tid, $feed['uid'], $this->user['uid'], 'comment', $_POST['content']);
+            return ['ret' => 0, 'status' => 'ok', 'tid' => $tid, 'cid' => $comment_id];
+        } else {
+            return ['ret' => 3001, 'status' => 'invalid tid'];
+        }
     }
 
-    /**
-     * @filter auth canFeed
-     */
-    function ac_like()
+    public function ac_like(LikeModel $likeModel, int $tid = 0): array
     {
-        $tid = $_REQUEST['tid'] ?? 0;
-        if (BUNNY_APP_MODE == BunnyPHP::MODE_API) {
-            $feedModel = new FeedModel();
-            if ($feed = $feedModel->getFeed($tid)) {
-                $tp_user = BunnyPHP::app()->get('tp_user');
-                if ((new LikeModel())->isLike($tp_user['uid'], 3, $tid) == 1) {
-                    $this->assignAll(['ret' => 3003, 'status' => 'already liked']);
-                } else {
-                    (new LikeModel())->like($tp_user['uid'], 3, $tid);
-                    $like_num = $feedModel->likeFeed($tid);
-                    (new NotificationModel())->notify(3, $tid, $feed['uid'], $tp_user['uid'], 'like', '');
-                    $this->assignAll(['ret' => 0, 'status' => 'ok', 'like_num' => $like_num]);
-                }
+        if ($feed = $this->feedModel->getFeed($tid)) {
+            if ($likeModel->isLike($this->user['uid'], ConstUtil::MOD_FEED, $tid) == 1) {
+                return ['ret' => 3003, 'status' => 'already liked'];
             } else {
-                $this->assignAll(['ret' => 3001, 'status' => 'invalid tid']);
+                $likeModel->like($this->user['uid'], ConstUtil::MOD_FEED, $tid);
+                $like_num = $this->feedModel->likeFeed($tid);
+                (new NotificationModel())->notify(ConstUtil::MOD_FEED, $tid, $feed['uid'], $this->user['uid'], 'like', '');
+                return ['ret' => 0, 'status' => 'ok', 'like_num' => $like_num];
             }
+        } else {
+            return ['ret' => 3001, 'status' => 'invalid tid'];
         }
-        $this->render();
     }
 
-    /**
-     * @filter auth canFeed
-     */
-    function ac_delete()
+    public function ac_delete(int $tid = 0): array
     {
-        $tid = $_REQUEST['tid'] ?? 0;
-        if (BUNNY_APP_MODE == BunnyPHP::MODE_API) {
-            $feedModel = new FeedModel();
-            if ($feed = $feedModel->getFeed($tid)) {
-                $tp_user = BunnyPHP::app()->get('tp_user');
-                if ($feed['username'] == $tp_user['username']) {
-                    $images = (new FeedImageModel())->getFeedImageByTid($tid);
-                    if ($images != null) {
-                        foreach ($images as $image) {
-                            BunnyPHP::getStorage()->remove($image['url']);
-                        }
+        if ($feed = $this->feedModel->getFeed($tid)) {
+            if ($feed['username'] == $this->user['username']) {
+                $images = (new FeedImageModel())->getFeedImageByTid($tid);
+                if ($images != null) {
+                    foreach ($images as $image) {
+                        BunnyPHP::getStorage()->remove($image['url']);
                     }
-                    (new FeedImageModel())->where('tid=:t', ['t' => $tid])->delete();
-                    (new CommentModel())->where('aid=3 and tid=:t', ['t' => $tid])->delete();
-                    (new LikeModel())->where('aid=3 and tid=:t', ['t' => $tid])->delete();
-                    $feedModel->where('tid=:t', ['t' => $tid])->delete();
-                    $this->assignAll(['ret' => 0, 'status' => 'ok']);
-                } else {
-                    $this->assignAll(['ret' => 3002, 'status' => 'permission denied']);
                 }
+                (new FeedImageModel())->where('tid=:t', ['t' => $tid])->delete();
+                (new CommentModel())->where('aid=3 and tid=:t', ['t' => $tid])->delete();
+                (new LikeModel())->where('aid=3 and tid=:t', ['t' => $tid])->delete();
+                $this->feedModel->where('tid=:t', ['t' => $tid])->delete();
+                return ['ret' => 0, 'status' => 'ok'];
             } else {
-                $this->assignAll(['ret' => 3001, 'status' => 'invalid tid']);
+                return ['ret' => 3002, 'status' => 'permission denied'];
             }
+        } else {
+            return ['ret' => 3001, 'status' => 'invalid tid'];
         }
-        $this->render();
     }
 }
