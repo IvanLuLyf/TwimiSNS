9546dd347db50fd1f82a9ebb2cc628cf7809719c
IvanLuLyf <835498692@qq.com>
2021-10-10 00:08:10 +0800

    add BunnyPHP-Mailer

--- app/service/EmailService.php
+++ app/service/EmailService.php
@@ -1,26 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: IvanLu
- * Date: 2019/2/28
- * Time: 21:50
- */
 
+use Bunny\Message\Mailer;
 use BunnyPHP\Config;
 use BunnyPHP\Service;
 use BunnyPHP\Template;
 
-require APP_PATH . "library/Mailer.php";
-
+/**
+ * @author IvanLu
+ * @time 2019/2/28 21:50
+ */
 class EmailService extends Service
 {
     public function sendMail($template, $context, $to, $title)
     {
-        if (Config::check("email")) {
-            $config = Config::load("email");
+        if (Config::check('email')) {
+            $config = Config::load('email');
             $mailer = new Mailer($config->get('host'), $config->get('port'), true, $config->get('username'), $config->get('password'));
             $body = Template::process($template, $context);
-            $mailer->sendMail($to, $config->get('email'), $title, $body, "HTML", TP_SITE_NAME);
+            $mailer->sendMail($to, $config->get('email'), $title, $body, 'HTML', TP_SITE_NAME);
         }
     }
 }
