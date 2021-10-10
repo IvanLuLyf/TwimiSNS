f84d068852bdb2f1e521c13780f6c3ada13ac7e9
IvanLuLyf <835498692@qq.com>
2021-10-11 00:25:56 +0800

    update footer

--- template/common/footer.php
+++ template/common/footer.php
@@ -1,6 +1,11 @@
 <footer>
     <div class="container mt-4">
-        <div class="center-block"><p class="text-center">TwimiSNS - Powered By <a
-                        href="https://github.com/IvanLuLyf/BunnyPHP">BunnyPHP</a></p></div>
+        <div class="center-block">
+            <p class="text-center">
+                <a href="https://github.com/IvanLuLyf/TwimiSNS">TwimiSNS</a>
+                - Powered By
+                <a href="https://github.com/IvanLuLyf/BunnyPHP">BunnyPHP</a>
+            </p>
+        </div>
     </div>
 </footer>
