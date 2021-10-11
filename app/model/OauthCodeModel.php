d802365db7e7fa98d497dd981ef478ac9c9e83e0
IvanLuLyf <835498692@qq.com>
2021-10-11 23:21:40 +0800

    update OauthCodeModel

--- app/model/OauthCodeModel.php
+++ app/model/OauthCodeModel.php
@@ -3,34 +3,32 @@
 use BunnyPHP\Model;
 
 /**
- * Created by PhpStorm.
- * User: IvanLu
- * Date: 2018/1/8
- * Time: 13:29
+ * @author IvanLu
+ * @time 2018/1/8 13:29
  */
 class OauthCodeModel extends Model
 {
-    protected $_column = [
+    protected array $_column = [
         'id' => ['integer', 'not null'],
         'uid' => ['integer', 'not null'],
-        'appid' => ['integer', 'not null'],
+        'app_id' => ['integer', 'not null'],
         'code' => ['text', 'not null'],
         'expire' => ['text']
     ];
-    protected $_pk = ['id'];
-    protected $_ai = 'id';
+    protected array $_pk = ['id'];
+    protected string $_ai = 'id';
 
     public function getCode($clientId, $appId, $uid, $timestamp): string
     {
         $code = md5($uid . $clientId . $timestamp);
-        $this->add(['uid' => $uid, 'appid' => $appId, 'code' => $code, 'expire' => ($timestamp + 604800)]);
+        $this->add(['uid' => $uid, 'app_id' => $appId, 'code' => $code, 'expire' => ($timestamp + 604800)]);
         return $code;
     }
 
     public function checkCode($appId, $appCode)
     {
-        if ($row = $this->where(['appid= ? and code= ? and expire > ?'], [$appId, $appCode, time()])->fetch()) {
-            $this->where(['appid= ? and code= ?'], [$appId, $appCode])->delete();
+        if ($row = $this->where(['app_id=? and code=? and expire>?'], [$appId, $appCode, time()])->fetch()) {
+            $this->where(['app_id=? and code=?'], [$appId, $appCode])->delete();
             return $row['uid'];
         } else {
             return null;
@@ -39,6 +37,6 @@ class OauthCodeModel extends Model
 
     public function deleteCode($appId, $appCode)
     {
-        return $this->where(['appid= ? and code= ?'], [$appId, $appCode])->delete();
+        return $this->where(['app_id=? and code=?'], [$appId, $appCode])->delete();
     }
 }
