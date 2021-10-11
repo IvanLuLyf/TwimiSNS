03055226b3fbfae1102a5ef1411f496bed8623ae
IvanLuLyf <835498692@qq.com>
2021-10-11 23:14:48 +0800

    update UserInfo

--- app/model/UserInfoModel.php
+++ app/model/UserInfoModel.php
@@ -3,20 +3,18 @@
 use BunnyPHP\Model;
 
 /**
- * Created by PhpStorm.
- * User: IvanLu
- * Date: 2019/2/28
- * Time: 23:17
+ * @author IvanLu
+ * @time 2019/2/28 23:17
  */
 class UserInfoModel extends Model
 {
-    protected $_column = [
+    protected array $_column = [
         'uid' => ['integer', 'not null'],
         'signature' => ['text'],
         'cover' => ['text'],
         'background' => ['text'],
     ];
-    protected $_pk = ['uid'];
+    protected array $_pk = ['uid'];
 
     public function get($uid)
     {
