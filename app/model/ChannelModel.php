51d78e5e9a38a44e1dd0b8232711c180cf1e120d
IvanLuLyf <835498692@qq.com>
2021-10-12 23:02:03 +0800

    update ChannelModel

--- app/model/ChannelModel.php
+++ app/model/ChannelModel.php
@@ -3,30 +3,28 @@
 use BunnyPHP\Model;
 
 /**
- * Created by PhpStorm.
- * User: IvanLu
- * Date: 2019/3/5
- * Time: 23:23
+ * @author IvanLu
+ * @time 2019/3/5 23:23
  */
 class ChannelModel extends Model
 {
-    protected $_column = [
+    protected array $_column = [
         'cid' => ['integer', 'not null'],
         'uid' => ['integer', 'not null'],
         'name' => ['text', 'not null'],
         'avatar' => ['text'],
         'description' => ['text']
     ];
-    protected $_pk = ['cid'];
-    protected $_ai = 'cid';
+    protected array $_pk = ['cid'];
+    protected string $_ai = 'cid';
 
     public function getChannelById($id)
     {
-        return $this->where("cid=:c", ['c' => $id])->fetch();
+        return $this->where('cid=:c', ['c' => $id])->fetch();
     }
 
     public function getChannelByName($name)
     {
-        return $this->where("name=:n", ['n' => $name])->fetch();
+        return $this->where('name=:n', ['n' => $name])->fetch();
     }
 }
