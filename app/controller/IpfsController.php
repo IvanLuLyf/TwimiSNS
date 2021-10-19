a79b70946110133b95b8e607b300b6cce11cc064
IvanLuLyf <835498692@qq.com>
2021-10-19 22:43:08 +0800

    TestIpfs

--- app/controller/IpfsController.php
+++ app/controller/IpfsController.php
@@ -1,6 +1,7 @@
 <?php
 
 
+use BunnyPHP\BunnyPHP;
 use BunnyPHP\Controller;
 
 class IpfsController extends Controller
@@ -12,6 +13,23 @@ class IpfsController extends Controller
     {
         $extra = $hash ? ('/' . implode('/', $hash)) : '';
         $path = BUNNY_ACTION . $extra;
-        var_dump($path);
+        $tag = md5($path);
+        $last_modified_time = BunnyPHP::getCache()->get('ipfs://' . $path);
+        if (!$last_modified_time) {
+            $last_modified_time = time();
+            BunnyPHP::getCache()->set('ipfs://' . $path, $last_modified_time);
+        }
+        $gmt_mtime = gmdate('r', $last_modified_time);
+        header('ETag: "' . $tag . '"');
+        header('Last-Modified: ' . $gmt_mtime);
+        header('Cache-Control: public');
+        if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) || isset($_SERVER['HTTP_IF_NONE_MATCH'])) {
+            if ($_SERVER['HTTP_IF_MODIFIED_SINCE'] == $gmt_mtime || str_replace('"', '', stripslashes($_SERVER['HTTP_IF_NONE_MATCH'])) == $tag) {
+                header('HTTP/1.1 304 Not Modified');
+                exit();
+            }
+        }
+        header('Content-type: application/octet-stream');
+        echo (new IpfsStorage([]))->read($path);
     }
 }
