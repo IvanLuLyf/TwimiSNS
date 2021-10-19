e905ed5c6618b2c26e91aa3f9b5f42e36274dcbe
IvanLuLyf <835498692@qq.com>
2021-10-19 22:55:54 +0800

    update Config

--- config/config.php
+++ config/config.php
@@ -10,6 +10,8 @@ return [
         'username' => $_ENV['DATABASE_USERNAME'] ?? '',
         'password' => $_ENV['DATABASE_PASSWORD'] ?? '',
     ],
+    'cache' => isset($_ENV['BUNNY_CACHE']) ? json_decode($_ENV['BUNNY_CACHE'], true) : [],
+    'storage' => isset($_ENV['BUNNY_STORAGE']) ? json_decode($_ENV['BUNNY_STORAGE'], true) : [],
     'site_name' => $_ENV['BUNNY_SITE_NAME'] ?? 'TwimiSNS',
     'site_url' => $_ENV['BUNNY_SITE_URL'] ?? 'localhost',
     'controller' => 'Index',
