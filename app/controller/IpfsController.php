2d7e0ada58c392e8827ca19aea66b774814eee96
IvanLuLyf <835498692@qq.com>
2021-10-20 23:07:51 +0800

    Check instance is IpfsStorage

--- app/controller/IpfsController.php
+++ app/controller/IpfsController.php
@@ -13,23 +13,28 @@ class IpfsController extends Controller
     {
         $extra = $hash ? ('/' . implode('/', $hash)) : '';
         $path = BUNNY_ACTION . $extra;
-        $tag = md5($path);
-        $last_modified_time = BunnyPHP::getCache()->get('ipfs://' . $path);
-        if (!$last_modified_time) {
-            $last_modified_time = time();
-            BunnyPHP::getCache()->set('ipfs://' . $path, $last_modified_time);
-        }
-        $gmt_mtime = gmdate('r', $last_modified_time);
-        header('ETag: "' . $tag . '"');
-        header('Last-Modified: ' . $gmt_mtime);
-        header('Cache-Control: public');
-        if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) || isset($_SERVER['HTTP_IF_NONE_MATCH'])) {
-            if ($_SERVER['HTTP_IF_MODIFIED_SINCE'] == $gmt_mtime || str_replace('"', '', stripslashes($_SERVER['HTTP_IF_NONE_MATCH'])) == $tag) {
-                header('HTTP/1.1 304 Not Modified');
-                exit();
+        $storage = BunnyPHP::getStorage();
+        if ($storage instanceof IpfsStorage) {
+            $tag = md5($path);
+            $last_modified_time = BunnyPHP::getCache()->get('ipfs://' . $path);
+            if (!$last_modified_time) {
+                $last_modified_time = time();
+                BunnyPHP::getCache()->set('ipfs://' . $path, $last_modified_time);
             }
+            $gmt_mtime = gmdate('r', $last_modified_time);
+            header('ETag: "' . $tag . '"');
+            header('Last-Modified: ' . $gmt_mtime);
+            header('Cache-Control: public');
+            if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) || isset($_SERVER['HTTP_IF_NONE_MATCH'])) {
+                if ($_SERVER['HTTP_IF_MODIFIED_SINCE'] == $gmt_mtime || str_replace('"', '', stripslashes($_SERVER['HTTP_IF_NONE_MATCH'])) == $tag) {
+                    header('HTTP/1.1 304 Not Modified');
+                    exit();
+                }
+            }
+            header('Content-type: image');
+            echo $storage->read($path);
+        } else {
+            $this->redirect("https://ipfs.io/ipfs/$path");
         }
-        header('Content-type: image');
-        echo (new IpfsStorage())->read($path);
     }
 }
