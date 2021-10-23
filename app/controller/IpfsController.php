d5fd0637835174b54c2733aa268d993c6a1a98a4
IvanLuLyf <835498692@qq.com>
2021-10-23 21:11:24 +0800

    ipfs not changed,so simplify it

--- app/controller/IpfsController.php
+++ app/controller/IpfsController.php
@@ -15,22 +15,13 @@ class IpfsController extends Controller
         $extra = $hash ? ('/' . implode('/', $hash)) : '';
         $path = BUNNY_ACTION . $extra;
         if ($storageName == 'ipfs') {
-            $tag = md5($path);
-            $last_modified_time = BunnyPHP::getCache()->get('ipfs://' . $path);
-            if (!$last_modified_time) {
-                $last_modified_time = time();
-                BunnyPHP::getCache()->set('ipfs://' . $path, $last_modified_time);
-            }
-            $gmt_mtime = gmdate('r', $last_modified_time);
-            header('ETag: "' . $tag . '"');
-            header('Last-Modified: ' . $gmt_mtime);
             header('Cache-Control: public');
             if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) || isset($_SERVER['HTTP_IF_NONE_MATCH'])) {
-                if ($_SERVER['HTTP_IF_MODIFIED_SINCE'] == $gmt_mtime || str_replace('"', '', stripslashes($_SERVER['HTTP_IF_NONE_MATCH'])) == $tag) {
-                    header('HTTP/1.1 304 Not Modified');
-                    exit();
-                }
+                header('HTTP/1.1 304 Not Modified');
+                exit();
             }
+            header('ETag: "' . md5($path) . '"');
+            header('Last-Modified: ' . gmdate('r', time()));
             header('Content-type: image');
             echo (BunnyPHP::getStorage())->read($path);
         } else {
