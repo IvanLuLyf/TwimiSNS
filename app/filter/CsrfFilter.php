e53e84db9b2f5d47c206a67f9e658f3438fa3c1a
IvanLuLyf <835498692@qq.com>
2021-10-23 20:35:11 +0800

    update CsrfFilter

--- app/filter/CsrfFilter.php
+++ app/filter/CsrfFilter.php
@@ -2,6 +2,7 @@
 
 use BunnyPHP\BunnyPHP;
 use BunnyPHP\Filter;
+use BunnyPHP\Request;
 
 /**
  * @author IvanLu
@@ -12,17 +13,17 @@ class CsrfFilter extends Filter
     public function doFilter($param = []): int
     {
         if (BUNNY_APP_MODE == BunnyPHP::MODE_NORMAL || BUNNY_APP_MODE == BunnyPHP::MODE_AJAX) {
-            $csrf_token = BunnyPHP::getRequest()->getSession('csrf_token');
+            $csrf_token = Request::session('csrf_token');
             if (in_array('check', $param)) {
                 if ($csrf_token && !empty($_POST['csrf_token']) && $_POST['csrf_token'] == $csrf_token) {
-                    unset($_SESSION['csrf_token']);
+                    Request::session('csrf_token', null);
                 } else {
                     $this->error(['ret' => 1, 'status' => 'invalid csrf token', 'tp_error_msg' => '非法的请求操作']);
                     return self::STOP;
                 }
             }
             $token = md5(time() . rand(1, 1000));
-            BunnyPHP::getRequest()->setSession('csrf_token', $token);
+            Request::session('csrf_token', $token);
             $this->assign('csrf_token', $token);
             return self::NEXT;
         } else {
