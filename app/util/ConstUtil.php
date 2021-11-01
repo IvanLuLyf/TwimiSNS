2eccebd39114f79bf6fd291662893acf58a6adbf
IvanLuLyf <835498692@qq.com>
2021-11-02 00:51:48 +0800

    add const

--- app/util/ConstUtil.php
+++ app/util/ConstUtil.php
@@ -8,4 +8,7 @@ class ConstUtil
     const APP_SYSTEM = 1;
     const APP_NORMAL = 2;
     const APP_THIRD_PARTY = 3;
+
+    const IMAGE_TYPES = ['image/bmp', 'image/gif', 'image/jpeg', 'image/pjpeg', 'image/png', 'application/x-bmp', 'application/x-jpg', 'application/x-png'];
+    const IMAGE_MAX_SIZE = 2000000;
 }
